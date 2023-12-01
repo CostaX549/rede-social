4e721c6488bc3e8f07b20389b205e6e0cb3f18b6
CostaX549 <138066007+CostaX549@users.noreply.github.com>
2023-12-01 13:21:35 -0300

    Mudança Login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -38,6 +38,9 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
     }
 
-    
+    public function username()
+    {
+        return 'username';
+    }
 
 }
